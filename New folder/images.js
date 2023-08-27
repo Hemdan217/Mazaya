2efc8fbd4b5581aef3872cb61f05bd05c4df5174
Mr Robot <hemdan219@gmail.com>
2023-08-27 23:51:57 +0300

    m:

---
+++ New folder/images.js
@@ -0,0 +1,15 @@
+import circle from "../assets/circle.svg";
+import logo from "../assets/Picture3.jpg";
+import logo2 from "../assets/logo2.jpeg";
+import img1 from "../assets/1.webp";
+import img2 from "../assets/2.jpg";
+import img3 from "../assets/3.jpg";
+
+export default {
+  circle,
+  logo,
+  logo2,
+  img1,
+  img2,
+  img3,
+};
